ad8544906d3482d504d0f03029c774d91525aa8c
mfelona <mfelona@fit.edu.ph>
2026-09-09 14:59:28 +0800

    Create BMICalculator.java

---
+++ Module3/BMICalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter weight in kilograms: ");
+        double weight = input.nextDouble();
+
+        System.out.print("Enter height in meters: ");
+        double height = input.nextDouble();
+
+        double bmi = weight / (height * height);
+
+        System.out.println("BMI: " + bmi);
+
+        input.close();
+    }
+}
